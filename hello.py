00d63ceca769fbf63f94f71a8ce16c85a9659099
CapeCos <cosmicquells@gmail.com>
2023-04-26 04:07:55 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 print ("this is a test")
 print ("CapeCos was here!")
 print ("CapeCos was here again - this time, code reviews might have been enabled?!")
-print ("CapeCos was here again - this time, limitations were  enabled?!")
-print ("CapeCos again! This time I was removed as a collaborator. Can I still push without review!?")
+print ("CapeCos was here again - this time, limitations were  enabled?! here's a change")
+print ("CapeCos again! This time I was removed as a collaborator. I can't push without review!")
+print ("meow")
