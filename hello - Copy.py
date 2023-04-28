49695debaee8f08af414866c86c50a1fd1a959d7
CapeCos <cosmicquells@gmail.com>
2023-04-28 03:46:05 -0700

    Create hello - Copy.py

---
+++ hello - Copy.py
@@ -0,0 +1,2 @@
+print ("this is a new file added via a new branch ToolsTest2b. CapeCos is now a collaborator. Project is public")
+print ("We think being 'public' is what grants us access to all of the tool's features")
